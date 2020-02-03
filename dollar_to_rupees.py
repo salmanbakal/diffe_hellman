9f9510e1b0e96d5d7e717145ff81b28958f984c9
salman_144 <47896980+salmanbakal@users.noreply.github.com>
2020-02-03 09:39:16 +0530

    Add files via upload

---
+++ dollar_to_rupees.py
@@ -0,0 +1,13 @@
+
+one_dollar = 71
+def dollor_rupees(one_rupee):
+    one_rupee = one_rupee*1 / one_dollar
+    return one_rupee
+
+def main():
+   
+    one_rupee = int(input("enter number of rupees!"))
+    print("thanks for coming!")
+    print(dollor_rupees(one_rupee))
+main()    
+    
